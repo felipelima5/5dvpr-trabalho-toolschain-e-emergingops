6063fcfe3d7b65efdcfb3c911efd054280a53ff3
Felipe Lima Silva <felipe.silva@selectsolucoes.com>
2024-09-20 22:07:42 -0300

    first commit to implement app and it infra

---
+++ tf-infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "tf-state-5dvpr-trabalho-toolschain-e-emergingops"
+    key     = "production/infra.tfstate"
+    encrypt = "true"
+    region  = "us-east-1"
+  }
+}  
