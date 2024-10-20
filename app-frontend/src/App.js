d403c15cf07e6a1327273206b8cf555cb382c492
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-10-20 13:27:36 -0300

    add app frontend

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+import React from 'react'; 
+import { Menubar } from 'primereact/menubar';
+
+export default function UnstyledDemo() {
+    const items = [
+        {
+            label: 'Membros da Equipe',
+            icon: 'pi pi-fw pi-file',
+            items: [
+                {
+                    label: 'Felipe Lima Silva',
+                    icon: 'pi pi-fw pi-trash'
+                },
+                {
+                  label: 'Adriano Silva',
+                  icon: 'pi pi-fw pi-trash'
+                },
+                {
+                  label: 'Giuliano Cintra',
+                  icon: 'pi pi-fw pi-trash'
+                },
+                {
+                    separator: true
+                },
+                {
+                  label: 'Sair',
+                  icon: 'pi pi-fw pi-trash'
+                }
+            ]
+        },
+        {
+            label: 'Turma',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {
+                    label: '5DVPR',
+                    icon: 'pi pi-fw pi-align-left'
+                }
+            ]
+        },
+        {
+            label: 'Curso',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'DevOps & Reliability Engineering',
+                    icon: 'pi pi-fw pi-user-plus',
+
+                } 
+            ]
+        },
+        {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off'
+        }
+    ];
+
+    return (
+        <div className="card">
+            <Menubar model={items} />
+        </div>
+    )
 }
 
-export default App;
