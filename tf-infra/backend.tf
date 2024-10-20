d35b7ddfd286bcce5e7d828af30342222769217c
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-10-20 11:32:30 -0300

    wip

--- tf-infra/backend.tf
+++ tf-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "tf-state-5dvpr-job-toolschain-e-emergingops"
+    bucket  = "tf-state-5dvpr-trabalho-toolschain-e-emergingops"
     key     = "infraestrutura.tfstate"
     encrypt = "true"
     region  = "us-east-2"
